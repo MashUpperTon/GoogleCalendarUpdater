5f07e12393a31670ed38a80024d80e0d8583642e
AllMashedUp <twitteronlyforbot@gmail.com>
2026-09-02 22:25:13 +0100

    Added a bunch of comments, also removed hard coded deltaCutoff

--- GoogleCalenderUpdater.py
+++ GoogleCalenderUpdater.py
@@ -11,10 +11,9 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import datetime
 import os.path
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
-deltaCutoff = 5
+SCOPES = ["https://www.googleapis.com/auth/calendar"] # What permissions you grant this program from the Google Calendar API
 
-def getConfig():
+def getConfig(): # Read the config file that contains http url, password and username, gc calendar ID and the "delta cutoff", which determines at what point to edit an event or just delete it and make a new one
     f = open("config.txt","rt")
     config = f.readlines()
     x = ast.literal_eval(config[0])
@@ -31,16 +30,16 @@ def getConfig():
     password = x["password"]
     deltaCutoff = x["deltaCutoff"]
     f.close()
-def logIt(Message, type):
+def logIt(Message, type): # Log to "logcal.txt" and print to output
     f = open("logcal.txt","at")
     f.write("{} [{}] {}\n".format(datetime.datetime.now(),type,Message))
     f.close()
     print(Message)
-def getUofGTimetable():
+def getUofGTimetable(): # Get ICS file from http server using Http basic authenticain
     newCalender = requests.get(downloadURL,auth=HTTPBasicAuth(username,password))
     return newCalender.text
 
-def getEvents(minTime):
+def getEvents(minTime): # Get google calendar events starting from minTime
     eventResults = (
         service.events()
         .list(
@@ -55,9 +54,9 @@ def getEvents(minTime):
     events = eventResults.get("items",[])
     return events
 
-def addEvent(title,description,location,start,end):
+def addEvent(title,description,location,start,end): # Add event to google calendar
     logIt("New event added \n Title = {},\n Description = {},\n Location = {},\n Start = {},\n End = {}".format(title,description,location,start,end),"INFO")
-    colourId = 1
+    colourId = 1 # Default value incase the below fails to find a colour
     for colour in colours:
         if re.search("(?i){}".format(colour),title):
             colourId = colours[colour]
@@ -76,21 +75,21 @@ def addEvent(title,description,location,start,end):
             "dateTime": end,
             "timeZone": "Europe/Belfast"
         },
-        "colorId": colourId
+        "colorId": colourId # sorry just wanted to confuse people with british spelling
     }
     # Add event addition to batch request
     batch.add(service.events().insert(calendarId=uniTimetableCalendarId, body=eventBody))
 
 
-def deleteEvent(id):
-    x = [y for y in EventsToBeRemoved if y["id"]==id]
+def deleteEvent(id): # Delete event from google calendar
+    x = [y for y in EventsToBeRemoved if y["id"]==id] # List comprehension to find the Event with the correct id, in order to get its properties for logging
     x = x[0]
     logIt("Event Deleted \n Id = {}, \n Title = {},\n Description = {},\n Location = {},\n Start = {},\n End = {}".format(id, x["summary"],x["description"],x["location"],x["start"]["dateTime"],x["end"]["dateTime"]),"INFO")
     batch.add(service.events().delete(calendarId=uniTimetableCalendarId, eventId=id))
 
-def editEvent(newTitle,newDescription,newLocation,newStart,newEnd,id):
+def editEvent(newTitle,newDescription,newLocation,newStart,newEnd,id): # Edit event in google calendar
     logIt("Event edited \n id = {}, \n Title = {},\n Description = {},\n Location = {},\n Start = {},\n End = {}".format(id, newTitle,newDescription,newLocation,newStart,newEnd),"INFO")
-    colourId = 1
+    colourId = 1 # Default value in case the below can't find a colour
     for colour in colours:
         if re.search("(?i){}".format(colour),newTitle):
             colourId = colours[colour]
@@ -114,7 +113,7 @@ def editEvent(newTitle,newDescription,newLocation,newStart,newEnd,id):
     # Add event change to batch request
     batch.add(service.events().update(calendarId=uniTimetableCalendarId, eventId=id, body=eventBody))
 
-def setupCreds(): # Setup all da crededitial mess 
+def setupCreds(): # Setup all da crededitialis mess, mostly copied from the python quickstart for the google calendar API 
     global creds 
     creds = None
     if os.path.exists("token.json"):
@@ -128,22 +127,22 @@ def setupCreds(): # Setup all da crededitial mess
         with open("token.json","w") as token:
             token.write(creds.to_json())
 
-def batchCallback(request_id, response, exception):
+def batchCallback(request_id, response, exception): # Function that is called on completion (sucess or error) of http request to google calendar API
     if exception:
         logIt("Whoops! Request ID {} failed!!: {}".format(request_id,exception),"ERROR")
     else:
         logIt("Fan dabby dosie! Request ID {} went great! Response: {}".format(request_id, response),"INFO")
-def delMinTimeICS(events, minTime):
+def delMinTimeICS(events, minTime): # Get rid of events preceeding minTime from an ICS data block
     events = [event for event in events.events if event["DTSTART"].dt > datetime.datetime.fromisoformat(minTime)]
     return events
-def compareICSGC(ICS,googlecalendar):
+def compareICSGC(ICS,googlecalendar): # compare an ICS and google calendar event to see if they are identical (in terms of title, description, location and start and stop time)
     if ICS["DTSTART"].dt != datetime.datetime.fromisoformat(googlecalendar["start"]["dateTime"]):return False
     elif ICS["DTEND"].dt != datetime.datetime.fromisoformat(googlecalendar["end"]["dateTime"]):return False
     elif ICS["SUMMARY"] != googlecalendar["summary"]:return False
     elif ICS["DESCRIPTION"] != googlecalendar["description"]:return False
     elif ICS["LOCATION"] != googlecalendar["location"]:return False
     else: return True 
-def calcDeltaICSGC(ICS,googlecalendar):
+def calcDeltaICSGC(ICS,googlecalendar): # compare an ICS and google calendar event and calculate how different they are
     delta = 0
     if ICS["DTSTART"].dt != datetime.datetime.fromisoformat(googlecalendar["start"]["dateTime"]): delta = delta + 1
     if ICS["DTEND"].dt != datetime.datetime.fromisoformat(googlecalendar["end"]["dateTime"]): delta = delta + 1
@@ -151,35 +150,41 @@ def calcDeltaICSGC(ICS,googlecalendar):
     if ICS["DESCRIPTION"] != googlecalendar["description"]: delta = delta + 1
     if ICS["LOCATION"] != googlecalendar["location"]: delta = delta + 1
     return delta
-if __name__ == "__main__":
+if __name__ == "__main__": # Main function
+    # Setup variables and google api creds
     setupCreds()
     getConfig()
     try:
         logIt("Starting...", "INFO")
         service = build("calendar","v3", credentials=creds) # Setup google calender api
-        newcalics = getUofGTimetable()
-        newcal = icalendar.Calendar.from_ical(newcalics) # get the latest timetable from UofG
-        newcal = delMinTimeICS(newcal,"2026-09-20T18:07:16.736812+00:00" )
-        oldcal = getEvents("2026-09-20T18:07:16.736812+00:00")
-        batch = service.new_batch_http_request(callback=batchCallback)
+        newcalics = getUofGTimetable() # get the latest timetable from UofG
+        newcal = icalendar.Calendar.from_ical(newcalics) # Convert text ics into iCalendar object
+        newcal = delMinTimeICS(newcal,"2026-09-20T18:07:16.736812+00:00" ) 
+        oldcal = getEvents("2026-09-20T18:07:16.736812+00:00") # Last two lines delete events starting before 2026 adademic year TODO: change this so it's managed by the config file
+        batch = service.new_batch_http_request(callback=batchCallback) # Start batch request
         EventsToBeAdded = newcal.copy()
         EventsToBeRemoved = oldcal.copy()
+        # Removes all the events that are identical between the two calendars 
         for oldevent in oldcal:
             for newevent in newcal:
                 if compareICSGC(newevent,oldevent):
                     EventsToBeAdded.remove(newevent)
                     EventsToBeRemoved.remove(oldevent)
+
+        # Edits events with differences below deltaCutoff
         for removeEvent in EventsToBeRemoved:
             for addedEvent in EventsToBeAdded:
                 if calcDeltaICSGC(addedEvent,removeEvent) < deltaCutoff:
                     editEvent(addedEvent["SUMMARY"],addedEvent["DESCRIPTION"],addedEvent["LOCATION"],addedEvent["DTSTART"],addedEvent["DTEND"],removeEvent["id"])
                     EventsToBeAdded.remove(addedEvent)
                     EventsToBeRemoved.remove(removeEvent)
+        # Removes remaining events
         for event in EventsToBeRemoved:
             deleteEvent(event["id"])
+        # Adds remaining events
         for event in EventsToBeAdded:
             addEvent(event["SUMMARY"],event["DESCRIPTION"],event["LOCATION"],event["DTSTART"],event["DTEND"])
-        batch.execute()
+        batch.execute() # Execute http batch request
         logIt("Finished!","INFO")
-    except HttpError as error:
+    except HttpError as error: # Called when the http request can't even go through
         logIt("Whoopsy Poopsy, a funny wunny error has occured! {}".format(error),"ERROR")
